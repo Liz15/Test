033e35e118e7481b1975c5bdf75fe0d20e4c7b01
liz15 <1079517763@qq.com>
2018-10-31 13:12:55 +0800

    add a plugins(echart)

---
+++ Test/js/index.js
@@ -0,0 +1,18 @@
+window.onload = function (ev) {
+    var show = document.getElementById("show");
+    var echart = echarts.init(show);
+    option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar'
+        }]
+    };
+    echart.setOption(option);
+};
